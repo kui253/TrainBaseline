7c8a6a52ab79e4d1a7bb4af19e68d337cb9b6e0a
kui253 <2533917115@qq.com>
2025-01-03 16:44:57 +0800

    test

--- sft_llama2.sh
+++ sft_llama2.sh
@@ -24,7 +24,6 @@ fi
 export CUDA_VISIBLE_DEVICES=0,1,2,4
 export NCCL_P2P_DISABLE=1
 torchrun --nproc_per_node=4 sft_model.py sft_config/llama2/config_r64_bz128_nocausal_ep3.json $output_dir > $log_dir/$cur_time.log 2>&1 &
-# export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6
 # 
 # torchrun --nproc_per_node=6 sft_model.py sft_config/llama3/config_r256_bz8_pubmedqa.json $output_dir > $log_dir/$cur_time.log 2>&1 &
 # torchrun --nproc_per_node=4 sft_model.py sft_config/llama3/config_r256_bz4_stack_exchange_paired.json $output_dir > $log_dir/$cur_time.log 2>&1 &
